e33cf4231c90f2456a01234c511d7d668e6d87e1
Adam Lynch <contact@adamlynch.ie>
2014-05-11 18:02:40 +0100

    Generation using Gulp

---
+++ content/index.md
@@ -0,0 +1,6 @@
+# Adam Lynch
+Coming soon
+
+- [GitHub](https://github.com/adam-lynch)
+- [@lynchy010](https://twitter.com/lynchy010)
+- [Email](mailto:contact@adamlynch.com)
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var swig = require('swig');
+var path = require('path');
+var es = require('event-stream');
+var site = {};
+var templates = {};
+
+module.exports = gulp;
+
+gulp.task('default', ['generate']);
+
+gulp.task('generate', ['get-templates'], function(){
+
+    gulp.src('./content/**/*.md')
+        .pipe($.marked())
+        .pipe($.ssg(site))
+        .pipe(es.map(function(file, cb) {
+            var output = swig.render(templates['index'], {
+                locals: {
+                    site: site,
+                    content: String(file.contents)
+                }
+            });
+
+            file.contents = new Buffer(output);
+            cb(null, file);
+        }))
+        .pipe($.htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('get-templates', function(){
+    templates = {};
+
+    gulp.src('./templates/**/*')
+        .pipe(es.map(function(file){
+            var name = path.basename(file.path).split('.')[0];
+            templates[name] = String(file.contents);
+        }));
+});
