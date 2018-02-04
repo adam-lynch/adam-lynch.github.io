209dcc46a12982c627e748f45b65ef568bdee221
Adam <contact@adamlynch.com>
2018-02-04 21:10:16 +0000

    Debug SSR issue

--- components/Article/Article.vue
+++ components/Article/Article.vue
@@ -7,9 +7,9 @@
 
       <div class="post-meta">
           <time class="post-blog-date">{{ prettyDate }}</time>
-          <span class="post-blog-title" v-if="article.original">
+          <!-- <span class="post-blog-title" v-if="article.original">
               Originally posted on <anchor :href="article.original.url" class="post-blog-link" :shouldTrackClicks="true">{{ article.original.blogName }}</anchor>
-          </span>
+          </span> -->
       </div>
 
       <!-- <div class="post-content" v-html="contents"></div> -->
